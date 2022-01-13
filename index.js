06a8544c5a55971d24b1d047d045a4ad769ca740
Rafael Molina <rafael@molina-aquino.com>
2022-01-13 16:42:25 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict'
+const { graphql, buildSchema } =require("graphql")
+const schema = buildSchema(`
+    type Query {
+        hello: String
+    }
+`)
+
+graphql(schema, '{hello}').then((result) => console.log(result))
